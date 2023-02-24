e4e257091357acb7f6f9fafa3e69359e4235aed8
Aryan-Bhatt-pro <aryan.bhatt@iiitb.ac.in>
2023-02-24 11:56:38 +0530

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -23,12 +23,64 @@ function App() {
     setName(profile?.name ?? 'No Profile')
   }
 
+  const initAccount = async () => {
+
+    const transactionId = await fcl.mutate({
+
+      cadence: `
+
+        import Profile from 0xProfile
+
+
+        transaction {
+
+          prepare(account: AuthAccount) {
+
+            // Only initialize the account if it hasn't already been initialized
+
+            if (!Profile.check(account.address)) {
+
+              // This creates and stores the profile in the user's account
+
+              account.save(<- Profile.new(), to: Profile.privatePath)
+
+
+              // This creates the public capability that lets applications read the profile's info
+
+              account.link<&Profile.Base{Profile.Public}>(Profile.publicPath, target: Profile.privatePath)
+
+            }
+
+          }
+
+        }
+
+      `,
+
+      payer: fcl.authz,
+
+      proposer: fcl.authz,
+      
+      authorizations: [fcl.authz],
+      limit: 50,
+      
+    })
+    
+    console.log(fcl.authz)
+
+    const transaction = await fcl.tx(transactionId).onceSealed()
+
+    console.log(transaction)
+
+  }
+
   const AuthedState = () => {
     return (
       <div>
         <div>Address: {user?.addr ?? "No Address"}</div>
         <div>Profile Name: {name ?? "--"}</div> {/* NEW */}
         <button onClick={sendQuery}>Send Query</button>
+        <button onClick={initAccount}>Init Account</button> {/* NEW */}
         <button onClick={fcl.unauthenticate}>Log Out</button>
       </div>
     )
@@ -45,10 +97,29 @@ function App() {
 
   return (
     <div>
-      {user.loggedIn
-        ? <AuthedState />
-        : <UnauthenticatedState />
-      }
+      <div>
+        {user.loggedIn
+          ? <AuthedState />
+          : <UnauthenticatedState />
+        }
+      </div>
+      
+      {/* <div>hello</div> */}
+
+      <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
+  <div className="max-w-5xl mx-auto px-4">
+    <div className="flex items-center justify-between h-16">
+      <span className="text-2xl text-gray-900 font-semibold">Logo</span>
+      <div className="flex space-x-4 text-gray-900">
+        <a href="#">Dashboard</a>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
     </div>
   );
 }
